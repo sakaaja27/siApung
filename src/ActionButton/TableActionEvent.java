0f3453d6153d3ea62e79f4f25ecb8d5ee665a5b0
Bramasta <bramastasaka9@gmail.com>
2024-12-08 18:49:44 +0700

    initial commit

---
+++ src/ActionButton/TableActionEvent.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ActionButton;
+
+/**
+ *
+ * @author sakab
+ */
+public interface TableActionEvent {
+     public void onEdit(int row);
+
+    public void onDelete(int row);
+    
+    
+
+//    public void onView(int row);
+}
